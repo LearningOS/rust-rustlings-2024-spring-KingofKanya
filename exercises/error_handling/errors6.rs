bf3c390e34b4c67401e6e59004e17594f5a3a5cd
红豆刨冰 <114462966+KingofKanya@users.noreply.github.com>
2024-04-15 17:28:33 +0800

    finish error_handling

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,7 +9,6 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -26,12 +25,16 @@ impl ParsePosNonzeroError {
     }
     // TODO: add another error conversion function here.
     // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError{
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    //s.parse() 返回的是 Ok，则 ? 操作符会将 Ok 中的值直接返回，如果返回的是 Err，则会提前返回这个 Err，并将它包含的错误值转换为 ParsePosNonzeroError 类型
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 

687ded78d8bd1c96841bbd607da19a81fc7f8451
红豆刨冰 <114462966+KingofKanya@users.noreply.github.com>
2024-04-15 17:41:11 +0800

    finish generics

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -6,14 +6,17 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Wrapper {
-    value: u32,
+struct Wrapper <T> {
+    value: T,
 }
 
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
+// 第一个 <T> 是泛型类型参数的声明，它表明 Wrapper 结构体是一个泛型类型，它可以接受任意类型的值作为其成员。
+
+// 第二个 <T> 出现在 new 方法的返回类型 Self 之前，它表示方法 new 也是一个泛型方法，它接受一个类型
+// 参数 T，并返回一个包含 T 类型值的 Wrapper<T> 实例。这个 <T> 的作用域仅限于方法 new 内部。
+impl <T> Wrapper <T> {
+    pub fn new(value: T) -> Self {
         Wrapper { value }
     }
 }

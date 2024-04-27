4bcab00dd63ef15ba1fbf31ebe3997bf4b231bfa
红豆刨冰 <114462966+KingofKanya@users.noreply.github.com>
2024-04-27 19:17:02 +0800

    finish macros

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -3,8 +3,9 @@
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+// macro_rules! 是Rust中用于定义宏的关键字
+// ()是匹配模式,这里为空
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
@@ -12,5 +13,5 @@ macro_rules! my_macro {
 }
 
 fn main() {
-    my_macro();
+    my_macro!();
 }
--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -3,12 +3,14 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     my_macro!();
 }
 
+// 将 my_macro 宏导出到当前模块的外部作用域
+// 这样 main 函数所在的模块就能够引用这个宏了
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
